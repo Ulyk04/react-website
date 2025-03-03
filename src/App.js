7d405edced7b3ceae5f556cb5128b8c0d02be2dd
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-03-03 22:14:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,31 @@ import Registration from './components/Registration';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './components/Dashboard';
+import {AuthProvider} from 'react-auth-kit';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' element = {<Main/>} />
-          <Route path='/about' element = {<About/>} />
-          <Route path='/desc' element = {<Desc />} />
-          <Route path='/organization' element = {<Organization />} />
-          <Route path='/registration' element = {<Registration/>} />
-          <Route path='/login' element = {<Login/>} />
-          <Route path='dashboard' element = { <ProtectedRoute> <Dashboard /> </ProtectedRoute> } />
-        </Routes>
-      </div>  
-    </Router>
+    <AuthProvider 
+			authStorageType="cookie" 
+			authStorageName="_auth_t" 
+			authTimeStorageName="_auth_time" 
+			stateStorageName="_auth_state" 
+			cookieDomain={window.location.hostname} 
+			cookieSecure={window.location.protocol === "https:"}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element = {<Main/>} />
+            <Route path='/about' element = {<About/>} />
+            <Route path='/desc' element = {<Desc />} />
+            <Route path='/organization' element = {<Organization />} />
+            <Route path='/registration' element = {<Registration/>} />
+            <Route path='/login' element = {<Login/>} />
+            <Route path='dashboard' element = { <ProtectedRoute> <Dashboard /> </ProtectedRoute> } />
+          </Routes>
+        </div>  
+      </Router>
+    </AuthProvider>  
   );
 }
 
