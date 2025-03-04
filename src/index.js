4dbb9ae7cd1e1b64e0a61fa9d00c7b4b9b1777a7
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-03-04 15:21:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import  AuthProvider  from "react-auth-kit";  
-import { BrowserRouter } from "react-router-dom"; 
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider
-    authType={"cookie"} 
-    authName={"_auth"}
-    cookieDomain={window.location.hostname}
-    cookieSecure={false}
-  >
-    <BrowserRouter> 
+
+  
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
-  </AuthProvider>
+  
+
 );
